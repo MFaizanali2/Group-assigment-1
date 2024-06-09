749c1325e4f68953a4b4769d5a729ca14bf3d052
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-06-09 14:15:22 +0500

    js line 16
    continue

---
+++ app.js
@@ -0,0 +1,16 @@
+let heading = document.getElementById("head");
+let text = document.getElementById("para");
+let image = document.getElementById("img1");
+
+function clickone(){
+     
+}
+
+function clicktwo(){
+    heading.innerHTML = `CSS`;
+    text.innerHTML = ``
+}
+
+function clickthree(){
+
+}
